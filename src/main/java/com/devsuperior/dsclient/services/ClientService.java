1373dcf5157903c8df390be44611ad2b67b85176
Juan Ignacio <juanpineyrob@gmail.com>
2024-12-27 12:11:51 -0300

    Client service

---
+++ src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -0,0 +1,55 @@
+package com.devsuperior.dsclient.services;
+
+import com.devsuperior.dsclient.dto.ClientDTO;
+import com.devsuperior.dsclient.entities.Client;
+import com.devsuperior.dsclient.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client result = clientRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Cliente no encontrado"));
+        return new ClientDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> results = clientRepository.findAll(pageable);
+        return results.map(ClientDTO::new);
+    }
+
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+        CopyDtoToEntity(dto, entity);
+        return new ClientDTO(entity);
+    }
+
+    public ClientDTO update(ClientDTO dto, Long id) {
+        Client client = clientRepository.getReferenceById(id);
+        CopyDtoToEntity(dto, client);
+        return new ClientDTO(client);
+    }
+
+    public void delete(Long id) {
+        clientRepository.deleteById(id);
+    }
+
+    private void CopyDtoToEntity(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+        entity = clientRepository.save(entity);
+    }
+}
