3834a3ac3b5487f25425eaf020da2fd4eb12460a
Juan Ignacio <juanpineyrob@gmail.com>
2025-01-24 19:32:25 -0300

    Client DTO

---
+++ src/main/java/com/devsuperior/dsclient/dto/ClientDTO.java
@@ -0,0 +1,63 @@
+package com.devsuperior.dsclient.dto;
+
+import com.devsuperior.dsclient.entities.Client;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
+
+public class ClientDTO {
+        private Long id;
+        @NotBlank(message = "Campo requerido")
+        private String name;
+        private String cpf;
+        private Double income;
+        @PastOrPresent
+        private LocalDate birthDate;
+        private Integer children;
+
+    public ClientDTO() {
+    }
+
+    public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public ClientDTO(Client client) {
+        this.id = client.getId();
+        this.name = client.getName();
+        this.cpf = client.getCpf();
+        this.income = client.getIncome();
+        this.birthDate = client.getBirthDate();
+        this.children = client.getChildren();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+}
